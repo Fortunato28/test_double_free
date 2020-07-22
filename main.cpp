cea3b6c628c9265ef24e260a8a68452bbf4456f7
Anton Sapunov <fort.sav.28@gmail.com>
2020-07-23 01:09:14 +0700

    Работает

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/config.hpp>
+
+using namespace std;
+namespace bpo=boost::program_options;
+
+static constexpr auto dms_default = "9999999";
+
+int main()
+{
+    string filename = "test.txt";
+    ofstream out_file(filename);
+    out_file << "dms path = 8888888";
+    out_file.flush();
+
+    bpo::options_description desc("Allowed options");
+    desc.add_options()
+        ("dms path", bpo::value<string>()->default_value(dms_default), "some shit");
+
+    bpo::variables_map vm;
+    bpo::store(bpo::parse_config_file<char>(filename.c_str(), desc), vm);
+    bpo::notify(vm);
+
+    string gotten_dms_path = vm["dms path"].as<string>();
+
+
+    printf("HERE %s\n", gotten_dms_path.c_str());
+    return 0;
+}
